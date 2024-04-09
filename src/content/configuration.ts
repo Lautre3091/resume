7c7da2c576df7a995e1fb5c954293c7aefe597e2
Gweltaz Gestin <gestin.gweltaz@protonmail.com>
2024-04-09 21:37:31 +0200

    feat: add cv

--- src/content/configuration.ts
+++ src/content/configuration.ts
@@ -3,53 +3,9 @@ import CV from "@/src/content/fr/cv/cv";
 import { Configuration } from "@/src/model/configuration.model";
 
 export const configuration: Configuration = new Configuration({
-  repoUrl: "https://github.com/abrioy/resume-nextjs-ssg",
+  repoUrl: "https://github.com/Lautre3091/resume",
 
   variants: [
-    {
-      locale: {
-        label: "English",
-        url: "en",
-        navigatorLanguage: /en/,
-        htmlLang: "en",
-        openGraph: "en_UK",
-      },
-      infos: {
-        anonymousName: "API",
-        firstName: "André",
-        lastName: "Pinot",
-        jobTitle: "House cat",
-        pageTitle: (variant) =>
-          `${variant.infos.firstName()} ${variant.infos.lastName()} - ${variant.infos.jobTitle()}`,
-        pageDesc: (variant) =>
-          `Resume of ${variant.infos.firstName()} ${variant.infos.lastName()}`,
-        fullName: (variant) =>
-          `${variant.infos.firstName()} ${variant.infos.lastName()}`,
-        location: "Rennes",
-      },
-      picture: {
-        url: "PINOT_Andre.jpg",
-        type: "image/jpeg",
-        alt: (variant) =>
-          `Photo of ${variant.infos.firstName()} ${variant.infos.lastName()}`,
-      },
-      socials: {
-        linkedIn: "linkedin.com/in/andrépinot",
-        github: "github.com/andrépinot",
-        gitlab: "gitlab.com/andrépinot",
-      },
-      documents: [
-        {
-          singlePage: true,
-          headerTitle: "CV",
-          pdfName: "CV_PINOT_Andre_house_cat.pdf",
-          hoverDownload: "Download resume",
-          hoverPrint: "Print resume",
-          component: Resume,
-        },
-      ],
-    },
-
     {
       locale: {
         label: "Français",
@@ -59,10 +15,10 @@ export const configuration: Configuration = new Configuration({
         openGraph: "fr_FR",
       },
       infos: {
-        anonymousName: "API",
-        firstName: "André",
-        lastName: "Pinot",
-        jobTitle: "Chat domestique",
+        anonymousName: "GGE",
+        firstName: "Gweltaz",
+        lastName: "Gestin",
+        jobTitle: "Tech Lead, Full-stack",
         pageTitle: (variant) =>
           `${variant.infos.firstName()} ${variant.infos.lastName()} - ${variant.infos.jobTitle()}`,
         pageDesc: (variant) =>
@@ -71,22 +27,16 @@ export const configuration: Configuration = new Configuration({
           `${variant.infos.firstName()} ${variant.infos.lastName()}`,
         location: "Rennes",
       },
-      picture: {
-        url: "PINOT_Andre.jpg",
-        type: "image/jpeg",
-        alt: (variant) =>
-          `Photo de ${variant.infos.firstName()} ${variant.infos.lastName()}`,
-      },
       socials: {
-        linkedIn: "linkedin.com/in/andrépinot",
-        github: "github.com/andrépinot",
-        gitlab: "gitlab.com/andrépinot",
+        linkedIn: "linkedin.com/in/gweltazgestin",
+        github: "github.com/Lautre3091",
+        gitlab: "gitlab.com/Lautre3091",
       },
       documents: [
         {
           singlePage: true,
           headerTitle: "CV",
-          pdfName: "CV_PINOT_Andre_chat_domestique.pdf",
+          pdfName: "CV_GESTIN_Gweltaz_tech_lead.pdf",
           hoverDownload: "Télécharger le CV",
           hoverPrint: "Imprimer le CV",
           component: Resume,
@@ -94,7 +44,7 @@ export const configuration: Configuration = new Configuration({
         {
           singlePage: false,
           headerTitle: "Dossier de compétence",
-          pdfName: "CV_PINOT_Andre_chat_domestique_dossier_de_competences.pdf",
+          pdfName: "CV_GESTIN_Gweltaz_tech_lead_dossier_de_competences.pdf",
           hoverDownload: "Télécharger le dossier de compétence",
           hoverPrint: "Imprimer le dossier de compétence",
           component: CV,
